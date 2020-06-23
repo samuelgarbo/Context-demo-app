d901438744b418e722ac927dc74b87641bf4395a
Samuel Garbo <samuel.garbo@gmail.com>
2020-06-23 17:04:48 +0300

    Consuming ThemeContext in Navbar and PageContent

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,19 +1,18 @@
 import React, { createContext, Component } from 'react';
 
-const ThemeContext = createContext();
+export const ThemeContext = createContext();
 
-class ThemeProvider extends Component {
+export class ThemeProvider extends Component {
     constructor(props){
         super(props);
         this.state = {isDarkMode: true}
     }
     render() {
         return (
-            <ThemeContext.Provider> value={{...this.state}}
+            <ThemeContext.Provider value={{...this.state}}> 
                 {this.props.children}
             </ThemeContext.Provider>
         );
     }
 }
 
-export ThemeProvider;
